1a6cbcc71308c57da4836a975b3d9630dc2cad41
Kunalkrsingh-10 <kunalkuma2115@gmail.com>
2025-01-09 09:59:33 +0530

    Darkmode switch added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header"
-
+import Providers from "./Provider";
 const inter=Inter({subsets :['latin']})
 export const metadata = {
   title: "IMDB clone",
@@ -11,11 +11,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={inter.className}
-      >
+      <body className={inter.className}> 
+        <Providers>
         <Header/>
         {children}
+        </Providers>
+        
       </body>
     </html>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import MenuIteam from './MenuIteam'
 import { AiTwotoneHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
 import Link from 'next/link'
+import DarkModeSwitch from './DarkModeSwitch';
 export default function Header() {
   return (
     <div className="flex justify-between p-3 max-w-6xl mx-auto">
@@ -12,6 +13,7 @@ export default function Header() {
         <MenuIteam title="about" address="/about" icon={BsFillInfoCircleFill}/> 
         </div>
         <div className="flex gap-1 items-center">
+            <DarkModeSwitch/>
         <Link href={"/"} className="bg-amber-500 text-2xl px-2 py-1 rounded-xl font-extrabold">IMDB</Link>
         <span className="text-xl hidden sm:inline">Clone</span>
         </div>
