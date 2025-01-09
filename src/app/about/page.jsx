67b5db64b13320d08cff5529e7a710f147596b3f
Kunalkrsingh-10 <kunalkuma2115@gmail.com>
2025-01-09 11:37:13 +0530

    about page created

---
+++ src/app/about/page.jsx
@@ -0,0 +1,9 @@
+export default function page() {
+  return (
+    <div className="max-w-6xl mx-auto px-4 space-y-4">
+        <h1 className="text-2xl font-medium text-amber-500">About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut consequatur vitae porro quibusdam tempore veniam qui ratione cum dolore sint, minus magni reprehenderit excepturi magnam quisquam fuga quae quasi molestias? Est cum nesciunt quos voluptatum eveniet magni fugit vitae dicta repellat maxime aspernatur nostrum in quibusdam architecto ipsam illo odit, saepe nihil fuga mollitia sit sunt ipsum doloremque quis? Reprehenderit earum eos sequi mollitia quia esse exercitationem! Culpa aliquid praesentium hic tempore consequatur dicta dolorem, accusamus fugiat ad alias architecto suscipit, minima ex distinctio illum eius perspiciatis voluptate ducimus, natus voluptatum inventore iste consequuntur excepturi pariatur? Culpa maiores eum explicabo?<br/>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At eius illum corrupti reiciendis vitae perferendis qui voluptatem nulla expedita deserunt enim, dolorem quisquam dolor ullam officiis assumenda unde error distinctio laborum, dicta quae ratione facilis libero! Maiores impedit dicta ab ipsa eos corrupti nam asperiores, necessitatibus ad perferendis. Molestias tempora quisquam, sunt iste voluptates, sequi odit nihil aspernatur pariatur consequuntur, maiores praesentium culpa nemo earum unde quia animi recusandae odio est necessitatibus quas mollitia incidunt blanditiis. Atque, sint nesciunt vitae quis assumenda aspernatur labore quae quisquam dolor, magni voluptatem a. Ipsam consequuntur minus odio enim sunt quod quo rem tempore!</p>
+    </div>
+  )
+}
